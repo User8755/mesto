2fa4ad0f9d5d8122fbe3e8f94355a9829b4efd42
Evgeny Kalinin <user8755@yandex.ru>
2022-10-28 22:02:21 +0500

    add js script

--- script.js
+++ script.js
@@ -1,15 +1,29 @@
-const btnOpenPopup = document.querySelector('.profile__btn-edit');
-const popup = document.querySelector('.popup')
-let btnPopupExit = document.querySelector('.popup__btn-exit')
+let popup = document.querySelector('.popup')
+let openPopup = document.querySelector('.profile__btn-edit');
+let exitPopup = document.querySelector('.popup__btn-exit')
+let Name = document.querySelector ('.profile__name');
+let Work = document.querySelector ('.profile__work')
+let Form = document.querySelector ('.popup__container');
+let nameInput = document.querySelector ('.popup__input_name');
+let workInput =  document.querySelector ('.popup__input_work');
 
-function activePopup () {
+function popupVisible () {
   popup.classList.add('popup_visible');
+  nameInput.value = Name.textContent;
+  workInput.value = Work.textContent;
 }
 
+function popupExit() {
+  popup.classList.remove('popup_visible')
+}
 
+function formSubmitHandler (evt) {
+  evt.preventDefault();
+  Name.textContent = `${nameInput.value}`;
+  Work.textContent = `${workInput.value}`;
+  popupExit();
+}
 
-//btnOpenPopup.addEventListener ('click', activePopup());
-
-btnPopupExit.addEventListener ('click', function() {popup.classList.remove('popup_visible')});
-
-btnOpenPopup.addEventListener ('click', function() {popup.classList.add('popup_visible')});
+Form.addEventListener('submit', formSubmitHandler);
+openPopup.addEventListener ('click', popupVisible);
+exitPopup.addEventListener ('click', popupExit);
