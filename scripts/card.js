c3941df0ee48539123b3383dc5e04dd06c97a31e
Evgeny Kalinin <user8755@yandex.ru>
2022-12-20 12:56:33 +0500

    bugFix

--- scripts/card.js
+++ scripts/card.js
@@ -16,18 +16,18 @@ _getCard() {
 };
 
 _likeCard() {
-  this._like = this._element.querySelector('.card__like');
-  this._like.addEventListener('click', ()=>{
-    this._like.classList.toggle('card__like_active');
-  });
+  this.classList.toggle('card__like_active');
 };
 
 _deleteCard() {
-  this._del = this._element.querySelector('.card__btn-delete');
-  this._del.addEventListener('click', ()=>{
-    this._del.closest('.card').remove();
-  });
+    this._element.closest('.card').remove();
 };
+
+
+_setEventListeners() {
+  this._element.querySelector('.card__like').addEventListener('click', this._likeCard);
+  this._element.querySelector('.card__btn-delete').addEventListener('click', () => this._deleteCard());
+}
 
 addData(item) {
   this._element = this._getCard();
@@ -35,8 +35,8 @@ addData(item) {
   this._cardImg.src = this._link; 
   this._cardImg.alt = this._name; 
   this._element.querySelector('.card__title').textContent = this._name;
-  this._cardImg.addEventListener('click', item)
-
+  this._cardImg.addEventListener('click', item);
+  this._setEventListeners();
   return this._element
 };
 
